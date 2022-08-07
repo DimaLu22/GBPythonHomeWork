0e31d578ca1127d75907909bac806109ad930c18
Dmitry Lushchik <dmitripl@mail.ru>
2022-08-07 22:34:36 +0300

    homework Code optimization Seminar №6

--- task2_1.py
+++ task2_1.py
@@ -1,13 +1,11 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 
 number = input('Введите вещественное число: ')
-sum = 0
-for i in range(len(number)):
-    if number[i] != ',':
-        sum += int(number[i])
-print(f'{number} -> {sum}')
+# sum = 0
+# for i in range(len(number)):
+#     if number[i] != ',':
+#         sum += int(number[i])
+# print(f'{number} -> {sum}')
 
-
-#Можно по другому, но мы так не проходили:)
-# n = sum(map(int, list(str(number).replace(',',''))))
-# print(f'{number} -> {n}')
+n = sum(map(int, list(str(number).replace(',',''))))
+print(f'{number} -> {n}')
--- task2_3.py
+++ task2_3.py
@@ -5,14 +5,20 @@ i = 1
 sum = 0
 listN = []
 
-def func(number: int):
-    res = round((1+1/number)**number, 3)
-    return res
+# def func(number: int):
+#     res = round((1+1/number)**number, 3)
+#     return res
 
+#listN = [func(i) for i in range(1, n+1)]
+
+# while i <= n:
+#     listN.append(func(i))
+#     sum += func(i)
+#     i += 1
+
+#Оптимизированный код
+print(f'Ряд: {[round((1+1/i)**i, 3) for i in range(1, n+1)]}')
 while i <= n:
-    listN.append(func(i))
-    sum += func(i)
+    sum += round((1+1/i)**i, 3)
     i += 1
-
-print(f'Ряд: {listN}')
 print(f'Сумма чисел ряда: {sum}')
